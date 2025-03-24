d67b3d41c2f586d8f9678eb4b43c997e3f7bbb89
kenneth474 <kennethkharel@icloud.com>
2025-03-24 17:00:24 -0500

    fixes to script

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/api/message', methods=['GET'])
 def get_message():
     return jsonify({'message': 'Hello from Flask!'}), 200
